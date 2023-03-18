60c91fc6f51987be2c82595e22e6a91d6eb5ae73
zanedma <zanedma@gmail.com>
2023-03-17 21:18:53 -0700

    basic server implementation

---
+++ server/chatmanager/chatmanager.go
@@ -0,0 +1,80 @@
+package chatmanager
+
+import (
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+type Message struct {
+	Message  string `json:"message"`
+	ClientId string `json:"clientId"`
+}
+
+type Manager struct {
+	clients   map[*websocket.Conn]bool
+	add       chan *websocket.Conn
+	remove    chan *websocket.Conn
+	broadcast chan *Message
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		clients:   make(map[*websocket.Conn]bool),
+		add:       make(chan *websocket.Conn),
+		remove:    make(chan *websocket.Conn),
+		broadcast: make(chan *Message),
+	}
+}
+
+func (instance *Manager) AddClient(conn *websocket.Conn) {
+	instance.add <- conn
+	go instance.listenForMessages(conn)
+	// TODO: send list of messages
+}
+
+func (instance *Manager) RemoveClient(conn *websocket.Conn) {
+	instance.remove <- conn
+}
+
+func (instance *Manager) listenForMessages(conn *websocket.Conn) {
+	log.Println("Listening for messages:", conn.RemoteAddr().String())
+	for {
+		var msg Message
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+			log.Println("Error reading json:", err.Error())
+			instance.remove <- conn
+			return
+		}
+		log.Println("Received message")
+		log.Println(msg)
+		instance.broadcast <- &msg
+	}
+}
+
+func (instance *Manager) Run() {
+	for {
+		select {
+		case client := <-instance.add:
+			log.Println("Adding client:", client.RemoteAddr().String())
+			instance.clients[client] = true
+		case client := <-instance.remove:
+			log.Println("Removing client:", client.RemoteAddr().String())
+			if _, ok := instance.clients[client]; ok {
+				delete(instance.clients, client)
+				client.Close()
+			} else {
+				log.Println("Error removing client", client.RemoteAddr().String(), ": client not found")
+			}
+		case message := <-instance.broadcast:
+			log.Println("Broadcasting: ", message)
+			for client := range instance.clients {
+				err := client.WriteJSON(message)
+				if err != nil {
+					log.Println("Error writing message to client", client.RemoteAddr().String(), ": ", err)
+				}
+			}
+		}
+	}
+}
---
+++ server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	chatmanager "beehive-chat/chatmanager"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+const (
+	connectionPassword = "password123"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func checkPassword(next http.Handler) http.Handler {
+	// TODO: not plain text password
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Header)
+		password := r.Header["X-Connection-Password"]
+		log.Println(password)
+		if len(password) != 1 || password[0] != connectionPassword {
+			http.Error(w, "invalid password", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	manager := chatmanager.NewManager()
+	go manager.Run()
+	mux := http.NewServeMux()
+	connectHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// TODO: make sure password is authenticate client
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Println("Error upgrading connection: ", err)
+			return
+		}
+		manager.AddClient(conn)
+	})
+	mux.Handle("/", checkPassword(connectHandler))
+
+	http.ListenAndServe("localhost:8081", mux)
+}
